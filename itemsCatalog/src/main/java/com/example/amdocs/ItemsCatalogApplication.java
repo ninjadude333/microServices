f80fa31954129f3b3e7fd4ef732bfdc169d9ac2b
David Gidony <david.gidony@gmail.com>
2018-08-14 18:44:20 +0300

    new project

--- itemsCatalog/src/main/java/com/example/amdocs/ItemsCatalogApplication.java
+++ itemsCatalog/src/main/java/com/example/amdocs/ItemsCatalogApplication.java
@@ -1,27 +1,52 @@
 package com.example.amdocs;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ItemsCatalogApplication {
 
+	final ApplicationContext context = new ClassPathXmlApplicationContext("items.xml");
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ItemsCatalogApplication.class, args);
 	}
-}
 
-@Configuration
-class Config {
-	
-//	@LoadBalanced
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	@RequestMapping("/rest/items/{IID}")
+	public String getItemById(@PathVariable String IID) {
+		singleItem item = (singleItem)context.getBean(IID);
+		return item.toString();	
 	}
+	
+	@RequestMapping("/rest/items")
+	public String getAllItems() {
+		int itemsCount = Arrays.asList(context.getBeanDefinitionNames()).size();
+		String returnValue = "";
+		
+		for (int i = 1; i <= itemsCount; i++) {
+			
+			singleItem item = (singleItem)context.getBean(Integer.toString(i));
+			returnValue = returnValue + item + "\n";
+		}
+		
+		return returnValue;
+	}
+
+	@RequestMapping("/rest/items/destroyer")
+	public void destroyer() {
+		ClassPathXmlApplicationContext cxt = (ClassPathXmlApplicationContext)context;
+		cxt.close();
+		}
+	
+	
 }
--- restEndPoint/src/main/java/com/example/amdocs/RestEndPointApplication.java
+++ restEndPoint/src/main/java/com/example/amdocs/RestEndPointApplication.java
@@ -3,25 +3,58 @@ package com.example.amdocs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RestEndPointApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestEndPointApplication.class, args);
 		
+			
+	}
+	
+	@Configuration
+	class Config {
 		
+//		@LoadBalanced
+		@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
 	}
 	
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	@GetMapping
+	@RequestMapping("/items")
 	public String getAllItems() {
-		String url = "http://item-catalog:8702/rest/items";
+		String url = "http://items-catalog:8701/rest/items";
 		return restTemplate.getForObject(url, String.class);
 		}
 	
-}
+	@RequestMapping("/items/{IID}")
+	public String getItemByID(int IID) {
+		String url = "http://items-catalog:8701/rest/items/" + IID;
+		return restTemplate.getForObject(url, String.class);
+		}
+	
+	@RequestMapping("/orders")
+	public String getAllOrders(int IID) {
+		String url = "http://orders-catalog:8702/rest/orders";
+		return restTemplate.getForObject(url, String.class);
+		}
+	
+	@RequestMapping("/orders/{IID}")
+	public String getOrderByID(int OID) {
+		String url = "http://orders-catalog:8702/rest/orders/" + OID;
+		return restTemplate.getForObject(url, String.class);
+		}
+	}
